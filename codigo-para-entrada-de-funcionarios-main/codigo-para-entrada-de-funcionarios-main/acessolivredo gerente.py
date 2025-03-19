ff622d673272f72e64d7c45586cb1484e4b67d75
Yna-ribe <ynae.ribeiro@outlook.com>
2025-03-19 08:34:17 -0300

    Site de verificação de identificação dos funcionarios da empresa

---
+++ codigo-para-entrada-de-funcionarios-main/codigo-para-entrada-de-funcionarios-main/acessolivredo gerente.py
@@ -0,0 +1,10 @@
+print('Sistema de controle de acesso do escritório')
+cargo_do_funcionario=input('qual seu cargo? ')
+dia_da_semana=input('qual o dia da semana? ')
+
+if(cargo_do_funcionario == 'gerente'):
+    print ('Acesso permitido',cargo_do_funcionario)
+elif(dia_da_semana == 'sabado' or dia_da_semana == 'domingo'):
+    print("Acesso negado", cargo_do_funcionario)
+else:
+    print ('Acesso permitido',cargo_do_funcionario)
