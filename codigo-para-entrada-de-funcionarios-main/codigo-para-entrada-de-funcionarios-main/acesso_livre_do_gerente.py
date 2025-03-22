cbf5a28e9809508ed67d6167c7974b04ab0920a0
Yna-ribe <164517195+Yna-ribe@users.noreply.github.com>
2025-03-22 21:17:53 +0000

    adição do banco de dados

--- codigo-para-entrada-de-funcionarios-main/codigo-para-entrada-de-funcionarios-main/acesso_livre_do_gerente.py
+++ codigo-para-entrada-de-funcionarios-main/codigo-para-entrada-de-funcionarios-main/acesso_livre_do_gerente.py
@@ -1,14 +1,16 @@
-print('Sistema de controle de acesso do escritório para: gerentes, estagiario e analista')
-cargo_do_funcionario=input('qual seu cargo? ')
-dia_da_semana=input('qual o dia da semana? ')
+print(
+    "Sistema de controle de acesso do escritório para: gerentes, estagiario e analista"
+)
+cargo_do_funcionario = input("qual seu cargo? ")
+dia_da_semana = input("qual o dia da semana? ")
 
-while(cargo_do_funcionario != "gerente" or "estagiario" or "analista"):
+while cargo_do_funcionario != "gerente" or "estagiario" or "analista":
     for i in range(3):
-        print('acesso negado, tente outra vez, você só tem {i}tentativas')
+        print("acesso negado, tente outra vez, você só tem {i}tentativas")
         break
-if(cargo_do_funcionario == 'gerente'):
-    print ('Acesso permitido',cargo_do_funcionario)
-elif(dia_da_semana == 'sabado' or dia_da_semana == 'domingo'):
+if cargo_do_funcionario == "gerente":
+    print("Acesso permitido", cargo_do_funcionario)
+elif dia_da_semana == "sabado" or dia_da_semana == "domingo":
     print("Acesso negado", cargo_do_funcionario)
 else:
-    print('Acesso permitido',cargo_do_funcionario)
+    print("Acesso permitido", cargo_do_funcionario)
--- codigo-para-entrada-de-funcionarios-main/codigo-para-entrada-de-funcionarios-main/acessolivredo gerente.py
+++ codigo-para-entrada-de-funcionarios-main/codigo-para-entrada-de-funcionarios-main/acessolivredo gerente.py
@@ -1,10 +1,10 @@
-print('Sistema de controle de acesso do escritório')
-cargo_do_funcionario=input('qual seu cargo? ')
-dia_da_semana=input('qual o dia da semana? ')
+print("Sistema de controle de acesso do escritório")
+cargo_do_funcionario = input("qual seu cargo? ")
+dia_da_semana = input("qual o dia da semana? ")
 
-if(cargo_do_funcionario == 'gerente'):
-    print ('Acesso permitido',cargo_do_funcionario)
-elif(dia_da_semana == 'sabado' or dia_da_semana == 'domingo'):
+if cargo_do_funcionario == "gerente":
+    print("Acesso permitido", cargo_do_funcionario)
+elif dia_da_semana == "sabado" or dia_da_semana == "domingo":
     print("Acesso negado", cargo_do_funcionario)
 else:
-    print ('Acesso permitido',cargo_do_funcionario)
+    print("Acesso permitido", cargo_do_funcionario)
